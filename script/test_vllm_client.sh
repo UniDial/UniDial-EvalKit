4a1fc0dd52889b0f11133e7d3ca1ec609765905f
hakunamatata <jia_qi_0217@163.com>
2026-02-12 16:27:13 +0800

    test:vllm

---
+++ script/test_vllm_client.sh
@@ -0,0 +1,1 @@
+PYTHONPATH=. python3 src/eval.py --dataset locomo --raw_data_dir "./raw_data/LoCoMo"  --base_url "http://localhost:8000/v1/" --do_generation --model_name Qwen3-8B
---
+++ script/vllm_server.sh
@@ -0,0 +1,3 @@
+python3 -m vllm.entrypoints.openai.api_server \
+        --model ./Models/Qwen3-8B \
+        --served-model-name "Qwen3-8B"
